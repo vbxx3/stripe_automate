96e5af970cab3d38d2c8734fe546dfbe1d919e80
Valentine Bobrovsky <vbabrouski@gmail.com>
2021-01-06 06:37:16 +0300

    Dockerfile updated

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ from aiohttp import ClientSession
 from urllib import parse
 from dotenv import load_dotenv
 
+import click
+
 load_dotenv()
 uvloop.install()
 
@@ -20,8 +22,12 @@ PASSWORD = os.getenv('PASSWORD')
 CAPTCHA_KEY = os.getenv('CAPTCHA_KEY')
 PAYMENT_ID = os.getenv('PAYMENT_ID')
 
+CHARGE = "ch_1I646ZL5RkOYbcxuPRIFM3BX"
+CREATED = 1609809179
 
-async def main():
+
+async def main(charge: str,
+               created: int):
 	async with ClientSession(headers={
 		'User-Agent': USER_AGENT
 	}) as session:
@@ -64,29 +70,109 @@ async def main():
 				async with session.post('https://dashboard.stripe.com/ajax/sessions',
 				                        headers=headers,
 				                        data=payload) as response:
+					session_cookie = str(response.headers).split('session=')[1].split('; domain')[0]
 					response = await response.json()
 			if "csrf_token" not in response:
 				print(response)
 				raise Exception('Unexpected response')
 			csrf = response['csrf_token']
 			api_key = response['session_api_key']
-			print("\n================ Credentials ================")
+			print("================ Credentials ================")
 			print(f'CSRF Token: {csrf}')
 			print(f'Session API Key: {api_key}')
+			print(f'Session cookie: {session_cookie}')
 			print(f'for email {response["email"]}')
 			print()
-		# Next steps to get Risk Insights
-		# stripe-account header will be flexible later
-		async with session.get(f'https://dashboard.stripe.com/v1/events?related_object={PAYMENT_ID}',  # Will be modified later
-		                       headers={
-			                       'Authorization': f'Bearer {api_key}',
-			                       'stripe-livemode': 'false',
-			                       'stripe-account': 'acct_1I1Tb5L5RkOYbcxu',
-			                       'Referer': 'https://dashboard.stripe.com/test/payments'
-		                       }
-		                       ) as response:
-			print('Related response: ', await response.read())
+		print({"variables": {"time": created, "input": {"charge": f'"{charge}"'}}})
+		payload = {"operationName": "RadarAllSignalsForChargeQuery",
+		           "variables": {"time": created, "input": {"charge": f'"{charge}"'}},
+		           "query": "query RadarAllSignalsForChargeQuery($time: Int!, $input: ApiRadarSignalsSignalsDataInput) "
+		                    "{\n  currentMerchant {\n    id\n    created\n    radarSettingsAtTime(time: $time) {\n     "
+		                    " highestRiskThreshold\n      elevatedRiskThreshold\n      __typename\n    }\n    "
+		                    "__typename\n  }\n  radarSignalsData(input: $input) {\n    amountMeanForCard\n    "
+		                    "amountStandardDeviationForCard\n    billingToIpDistanceInKm\n    "
+		                    "shippingToBillingDistanceInKm\n    shippingToIpDistanceInKm\n    amountInUsd\n    "
+		                    "issuingBank\n    email\n    cardName\n    deviceBrand\n    deviceModel\n    emailAge\n    "
+		                    "internetServiceProvidor\n    cardholderIp\n    operatingSystem\n    browser\n    "
+		                    "cardholderName\n    cardCountsForDeviceLastWeek {\n      counterValue\n      insightsData "
+		                    "{\n        ...CounterFeatureInsightsDataFragment\n        __typename\n      }\n      "
+		                    "selectedBucket\n      __typename\n    }\n    cardCountsForDeviceSecondsSinceFirstSeen {\n "
+		                    "     counterValue\n      insightsData {\n        ...CounterFeatureInsightsDataFragment\n  "
+		                    "      __typename\n      }\n      selectedBucket\n      __typename\n    }\n    "
+		                    "cardCountsForEmailDeclinesLastWeek {\n      counterValue\n      insightsData {\n        "
+		                    "...CounterFeatureInsightsDataFragment\n        __typename\n      }\n      "
+		                    "selectedBucket\n      __typename\n    }\n    "
+		                    "cardCountsForEmailDeclinesSecondsSinceFirstSeen {\n      counterValue\n      insightsData "
+		                    "{\n        ...CounterFeatureInsightsDataFragment\n        __typename\n      }\n      "
+		                    "selectedBucket\n      __typename\n    }\n    cardCountsForIpLastWeek {\n      "
+		                    "counterValue\n      insightsData {\n        ...CounterFeatureInsightsDataFragment\n       "
+		                    " __typename\n      }\n      selectedBucket\n      __typename\n    }\n    "
+		                    "chargesForIpAllTimeSuccessRate {\n      counterValue\n      insightsData {\n        "
+		                    "...CounterFeatureInsightsDataFragment\n        __typename\n      }\n      "
+		                    "selectedBucket\n      __typename\n    }\n    ipCountsForCardAuthorizedLastWeek {\n      "
+		                    "counterValue\n      insightsData {\n        ...CounterFeatureInsightsDataFragment\n       "
+		                    " __typename\n      }\n      selectedBucket\n      __typename\n    }\n    "
+		                    "ipCountsForCardAuthorizedSecondsSinceFirstSeen {\n      counterValue\n      insightsData "
+		                    "{\n        ...CounterFeatureInsightsDataFragment\n        __typename\n      }\n      "
+		                    "selectedBucket\n      __typename\n    }\n    nameCountsForIpLastWeek {\n      "
+		                    "counterValue\n      insightsData {\n        ...CounterFeatureInsightsDataFragment\n       "
+		                    " __typename\n      }\n      selectedBucket\n      __typename\n    }\n    "
+		                    "nameCountsForIpSecondsSinceFirstSeen {\n      counterValue\n      insightsData {\n        "
+		                    "...CounterFeatureInsightsDataFragment\n        __typename\n      }\n      "
+		                    "selectedBucket\n      __typename\n    }\n    normalizedNameCountsForCardLastWeek {\n      "
+		                    "counterValue\n      insightsData {\n        ...CounterFeatureInsightsDataFragment\n       "
+		                    " __typename\n      }\n      selectedBucket\n      __typename\n    }\n    "
+		                    "countryCountsForCardLastWeek {\n      counterValue\n      insightsData {\n        "
+		                    "...CounterFeatureInsightsDataFragment\n        __typename\n      }\n      "
+		                    "selectedBucket\n      __typename\n    }\n    chargesForEmailAllTimeSuccessRate {\n      "
+		                    "counterValue\n      insightsData {\n        ...CounterFeatureInsightsDataFragment\n       "
+		                    " __typename\n      }\n      selectedBucket\n      __typename\n    }\n    isDebit {\n      "
+		                    "value\n      insightsData {\n        ...CategoricalFeatureInsightsDataFragment\n        "
+		                    "__typename\n      }\n      __typename\n    }\n    isPrepaid {\n      value\n      "
+		                    "insightsData {\n        ...CategoricalFeatureInsightsDataFragment\n        __typename\n   "
+		                    "   }\n      __typename\n    }\n    pastedCvc {\n      value\n      insightsData {\n       "
+		                    " ...CategoricalFeatureInsightsDataFragment\n        __typename\n      }\n      "
+		                    "__typename\n    }\n    pastedEmail {\n      value\n      insightsData {\n        "
+		                    "...CategoricalFeatureInsightsDataFragment\n        __typename\n      }\n      "
+		                    "__typename\n    }\n    pastedExpiration {\n      value\n      insightsData {\n        "
+		                    "...CategoricalFeatureInsightsDataFragment\n        __typename\n      }\n      "
+		                    "__typename\n    }\n    pastedNumber {\n      value\n      insightsData {\n        "
+		                    "...CategoricalFeatureInsightsDataFragment\n        __typename\n      }\n      "
+		                    "__typename\n    }\n    pastedZip {\n      value\n      insightsData {\n        "
+		                    "...CategoricalFeatureInsightsDataFragment\n        __typename\n      }\n      "
+		                    "__typename\n    }\n    isNamePartInEmail {\n      value\n      insightsData {\n        "
+		                    "...CategoricalFeatureInsightsDataFragment\n        __typename\n      }\n      "
+		                    "__typename\n    }\n    hasPriorDisputeOnCardholderIp {\n      value\n      insightsData {"
+		                    "\n        ...CategoricalFeatureInsightsDataFragment\n        __typename\n      }\n      "
+		                    "__typename\n    }\n    hasPriorIfrOnCardholderIp {\n      value\n      insightsData {\n   "
+		                    "     ...CategoricalFeatureInsightsDataFragment\n        __typename\n      }\n      "
+		                    "__typename\n    }\n    __typename\n  }\n}\n\nfragment CounterFeatureInsightsDataFragment "
+		                    "on ApiRadarSignalsCounterSignalBucket {\n  key\n  networkDistribution\n  "
+		                    "normalizedFraudRate\n  __typename\n}\n\nfragment CategoricalFeatureInsightsDataFragment "
+		                    "on ApiRadarSignalsCategoricalSignalBucket {\n  bucketValue\n  networkDistribution\n  "
+		                    "normalizedFraudRate\n  __typename\n}\n"}
+		async with session.post('https://dashboard.stripe.com/ajax/graphql',
+		                        json=payload,
+		                        headers={
+			                        'x-stripe-csrf-token': csrf,
+			                        'stripe-livemode': 'false',
+			                        'Referer': 'https://dashboard.stripe.com/test/payments/pi_1I646ZL5RkOYbcxulqDwH6RR',
+			                        'Origin': 'https://dashboard.stripe.com'
+		                        },
+		                        cookies={
+			                        'session': session_cookie
+		                        }
+		                        ) as response:
+			print(await response.json())
+
+
+@click.command()
+@click.option('--charge', default=CHARGE)
+@click.option('--created', type=int, default=CREATED)
+def click_main(charge: str,
+               created: int):
+	asyncio.get_event_loop().run_until_complete(main(charge, created))
 
 
 if __name__ == "__main__":
-	asyncio.get_event_loop().run_until_complete(main())
+	click_main()
