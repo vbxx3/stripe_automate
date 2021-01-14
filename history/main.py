7499b06ab453d70bf6c66ac9ec5e429c607b5aee
vbxx3 <vbabrouski@outlook.com>
2021-01-14 15:43:51 +0300

    Update

---
+++ history/main.py
@@ -0,0 +1,176 @@
+import os
+
+from recaptcha import Captcha
+
+import uvloop
+import asyncio
+
+from aiohttp import ClientSession
+from urllib import parse
+from dotenv import load_dotenv
+
+import click
+
+load_dotenv()
+uvloop.install()
+
+# Environment vars
+DATA_SITEKEY = '6LezRwYTAAAAAClbeZahYjeSYHsbwpzjEQ0hQ1jB'
+USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'
+EMAIL = os.getenv('EMAIL')
+PASSWORD = os.getenv('PASSWORD')
+CAPTCHA_KEY = os.getenv('CAPTCHA_KEY')
+
+CHARGE = "ch_1I646ZL5RkOYbcxuPRIFM3BX"
+CREATED = 1609809179
+
+
+async def main(charge: str,
+               created: int):
+	async with ClientSession(headers={
+		'User-Agent': USER_AGENT
+	}) as session:
+		async with session.get('https://dashboard.stripe.com/login',
+		                       ) as response:
+			await response.read()
+			unesc_csrf = str(response.headers).split('stripe.csrf=')[1].split('; domain=stripe.com')[0]
+			csrf = parse.unquote(unesc_csrf)
+		# Defining login payload
+		payload = {
+			"email": EMAIL,
+			"password": PASSWORD,
+			"io_blackbox": "",
+			"remember": True,
+			"merchant": "",
+			"invite_code": "",
+			"account_invite": "",
+			"invite": "",
+			"redirect": "/",
+			"source": "main_login",
+			"has_platform_authenticator": False
+		}
+		# Defining headers
+		headers = {
+			'origin': 'https://dashboard.stripe.com',
+			'referer': 'https://dashboard.stripe.com/login',
+			'x-stripe-csrf-token': csrf,
+			'x-requested-with': 'XMLHttpRequest'
+		}
+		async with session.post('https://dashboard.stripe.com/ajax/sessions',
+		                        headers=headers,
+		                        data=payload) as response:
+			response = await response.json()
+			if response.get('error_type', '') == 'need_captcha':
+				# Solve captcha if it raised
+				captcha = Captcha(CAPTCHA_KEY)
+				g_response = captcha.solve('https://dashboard.stripe.com/login',
+				                           DATA_SITEKEY)
+				payload['g-recaptcha-response'] = g_response
+				async with session.post('https://dashboard.stripe.com/ajax/sessions',
+				                        headers=headers,
+				                        data=payload) as response:
+					session_cookie = str(response.headers).split('session=')[1].split('; domain')[0]
+					response = await response.json()
+			if "csrf_token" not in response:
+				print(response)
+				raise Exception('Unexpected response')
+			csrf = response['csrf_token']
+			api_key = response['session_api_key']
+			print("================ Credentials ================")
+			print(f'CSRF Token: {csrf}')
+			print(f'Session API Key: {api_key}')
+			print(f'Session cookie: {session_cookie}')
+			print(f'for email {response["email"]}')
+			print()
+		payload = {"operationName": "RadarAllSignalsForChargeQuery",
+		           "variables": {"time": created, "input": {"charge": f'"{charge}"'}},
+		           "query": "query RadarAllSignalsForChargeQuery($time: Int!, $input: ApiRadarSignalsSignalsDataInput) "
+		                    "{\n  currentMerchant {\n    id\n    created\n    radarSettingsAtTime(time: $time) {\n     "
+		                    " highestRiskThreshold\n      elevatedRiskThreshold\n      __typename\n    }\n    "
+		                    "__typename\n  }\n  radarSignalsData(input: $input) {\n    amountMeanForCard\n    "
+		                    "amountStandardDeviationForCard\n    billingToIpDistanceInKm\n    "
+		                    "shippingToBillingDistanceInKm\n    shippingToIpDistanceInKm\n    amountInUsd\n    "
+		                    "issuingBank\n    email\n    cardName\n    deviceBrand\n    deviceModel\n    emailAge\n    "
+		                    "internetServiceProvidor\n    cardholderIp\n    operatingSystem\n    browser\n    "
+		                    "cardholderName\n    cardCountsForDeviceLastWeek {\n      counterValue\n      insightsData "
+		                    "{\n        ...CounterFeatureInsightsDataFragment\n        __typename\n      }\n      "
+		                    "selectedBucket\n      __typename\n    }\n    cardCountsForDeviceSecondsSinceFirstSeen {\n "
+		                    "     counterValue\n      insightsData {\n        ...CounterFeatureInsightsDataFragment\n  "
+		                    "      __typename\n      }\n      selectedBucket\n      __typename\n    }\n    "
+		                    "cardCountsForEmailDeclinesLastWeek {\n      counterValue\n      insightsData {\n        "
+		                    "...CounterFeatureInsightsDataFragment\n        __typename\n      }\n      "
+		                    "selectedBucket\n      __typename\n    }\n    "
+		                    "cardCountsForEmailDeclinesSecondsSinceFirstSeen {\n      counterValue\n      insightsData "
+		                    "{\n        ...CounterFeatureInsightsDataFragment\n        __typename\n      }\n      "
+		                    "selectedBucket\n      __typename\n    }\n    cardCountsForIpLastWeek {\n      "
+		                    "counterValue\n      insightsData {\n        ...CounterFeatureInsightsDataFragment\n       "
+		                    " __typename\n      }\n      selectedBucket\n      __typename\n    }\n    "
+		                    "chargesForIpAllTimeSuccessRate {\n      counterValue\n      insightsData {\n        "
+		                    "...CounterFeatureInsightsDataFragment\n        __typename\n      }\n      "
+		                    "selectedBucket\n      __typename\n    }\n    ipCountsForCardAuthorizedLastWeek {\n      "
+		                    "counterValue\n      insightsData {\n        ...CounterFeatureInsightsDataFragment\n       "
+		                    " __typename\n      }\n      selectedBucket\n      __typename\n    }\n    "
+		                    "ipCountsForCardAuthorizedSecondsSinceFirstSeen {\n      counterValue\n      insightsData "
+		                    "{\n        ...CounterFeatureInsightsDataFragment\n        __typename\n      }\n      "
+		                    "selectedBucket\n      __typename\n    }\n    nameCountsForIpLastWeek {\n      "
+		                    "counterValue\n      insightsData {\n        ...CounterFeatureInsightsDataFragment\n       "
+		                    " __typename\n      }\n      selectedBucket\n      __typename\n    }\n    "
+		                    "nameCountsForIpSecondsSinceFirstSeen {\n      counterValue\n      insightsData {\n        "
+		                    "...CounterFeatureInsightsDataFragment\n        __typename\n      }\n      "
+		                    "selectedBucket\n      __typename\n    }\n    normalizedNameCountsForCardLastWeek {\n      "
+		                    "counterValue\n      insightsData {\n        ...CounterFeatureInsightsDataFragment\n       "
+		                    " __typename\n      }\n      selectedBucket\n      __typename\n    }\n    "
+		                    "countryCountsForCardLastWeek {\n      counterValue\n      insightsData {\n        "
+		                    "...CounterFeatureInsightsDataFragment\n        __typename\n      }\n      "
+		                    "selectedBucket\n      __typename\n    }\n    chargesForEmailAllTimeSuccessRate {\n      "
+		                    "counterValue\n      insightsData {\n        ...CounterFeatureInsightsDataFragment\n       "
+		                    " __typename\n      }\n      selectedBucket\n      __typename\n    }\n    isDebit {\n      "
+		                    "value\n      insightsData {\n        ...CategoricalFeatureInsightsDataFragment\n        "
+		                    "__typename\n      }\n      __typename\n    }\n    isPrepaid {\n      value\n      "
+		                    "insightsData {\n        ...CategoricalFeatureInsightsDataFragment\n        __typename\n   "
+		                    "   }\n      __typename\n    }\n    pastedCvc {\n      value\n      insightsData {\n       "
+		                    " ...CategoricalFeatureInsightsDataFragment\n        __typename\n      }\n      "
+		                    "__typename\n    }\n    pastedEmail {\n      value\n      insightsData {\n        "
+		                    "...CategoricalFeatureInsightsDataFragment\n        __typename\n      }\n      "
+		                    "__typename\n    }\n    pastedExpiration {\n      value\n      insightsData {\n        "
+		                    "...CategoricalFeatureInsightsDataFragment\n        __typename\n      }\n      "
+		                    "__typename\n    }\n    pastedNumber {\n      value\n      insightsData {\n        "
+		                    "...CategoricalFeatureInsightsDataFragment\n        __typename\n      }\n      "
+		                    "__typename\n    }\n    pastedZip {\n      value\n      insightsData {\n        "
+		                    "...CategoricalFeatureInsightsDataFragment\n        __typename\n      }\n      "
+		                    "__typename\n    }\n    isNamePartInEmail {\n      value\n      insightsData {\n        "
+		                    "...CategoricalFeatureInsightsDataFragment\n        __typename\n      }\n      "
+		                    "__typename\n    }\n    hasPriorDisputeOnCardholderIp {\n      value\n      insightsData {"
+		                    "\n        ...CategoricalFeatureInsightsDataFragment\n        __typename\n      }\n      "
+		                    "__typename\n    }\n    hasPriorIfrOnCardholderIp {\n      value\n      insightsData {\n   "
+		                    "     ...CategoricalFeatureInsightsDataFragment\n        __typename\n      }\n      "
+		                    "__typename\n    }\n    __typename\n  }\n}\n\nfragment CounterFeatureInsightsDataFragment "
+		                    "on ApiRadarSignalsCounterSignalBucket {\n  key\n  networkDistribution\n  "
+		                    "normalizedFraudRate\n  __typename\n}\n\nfragment CategoricalFeatureInsightsDataFragment "
+		                    "on ApiRadarSignalsCategoricalSignalBucket {\n  bucketValue\n  networkDistribution\n  "
+		                    "normalizedFraudRate\n  __typename\n}\n"}
+		async with session.post('https://dashboard.stripe.com/ajax/graphql',
+		                        json=payload,
+		                        headers={
+			                        'x-stripe-csrf-token': csrf,
+			                        'stripe-livemode': 'false',
+			                        'Referer': 'https://dashboard.stripe.com/test/payments/pi_1I646ZL5RkOYbcxulqDwH6RR',
+			                        'Origin': 'https://dashboard.stripe.com'
+		                        },
+		                        cookies={
+			                        'session': session_cookie
+		                        }
+		                        ) as response:
+			print(await response.json())
+
+
+@click.command()
+@click.option('--charge', default=CHARGE)
+@click.option('--created', type=int, default=CREATED)
+def click_main(charge: str,
+               created: int):
+	asyncio.get_event_loop().run_until_complete(main(charge, created))
+
+
+if __name__ == "__main__":
+	click_main()
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 
+from payloads import risk_graphql
 from recaptcha import Captcha
+from utils import split
 
 import uvloop
 import asyncio
@@ -8,8 +10,6 @@ import asyncio
 from aiohttp import ClientSession
 from urllib import parse
 from dotenv import load_dotenv
-
-import click
 
 load_dotenv()
 uvloop.install()
@@ -25,152 +25,119 @@ CHARGE = "ch_1I646ZL5RkOYbcxuPRIFM3BX"
 CREATED = 1609809179
 
 
-async def main(charge: str,
-               created: int):
-	async with ClientSession(headers={
-		'User-Agent': USER_AGENT
-	}) as session:
-		async with session.get('https://dashboard.stripe.com/login',
-		                       ) as response:
-			await response.read()
-			unesc_csrf = str(response.headers).split('stripe.csrf=')[1].split('; domain=stripe.com')[0]
-			csrf = parse.unquote(unesc_csrf)
-		# Defining login payload
-		payload = {
-			"email": EMAIL,
-			"password": PASSWORD,
-			"io_blackbox": "",
-			"remember": True,
-			"merchant": "",
-			"invite_code": "",
-			"account_invite": "",
-			"invite": "",
-			"redirect": "/",
-			"source": "main_login",
-			"has_platform_authenticator": False
-		}
-		# Defining headers
-		headers = {
-			'origin': 'https://dashboard.stripe.com',
-			'referer': 'https://dashboard.stripe.com/login',
-			'x-stripe-csrf-token': csrf,
-			'x-requested-with': 'XMLHttpRequest'
-		}
-		async with session.post('https://dashboard.stripe.com/ajax/sessions',
-		                        headers=headers,
-		                        data=payload) as response:
-			response = await response.json()
-			if response.get('error_type', '') == 'need_captcha':
-				# Solve captcha if it raised
-				captcha = Captcha(CAPTCHA_KEY)
-				g_response = captcha.solve('https://dashboard.stripe.com/login',
-				                           DATA_SITEKEY)
-				payload['g-recaptcha-response'] = g_response
-				async with session.post('https://dashboard.stripe.com/ajax/sessions',
-				                        headers=headers,
-				                        data=payload) as response:
-					session_cookie = str(response.headers).split('session=')[1].split('; domain')[0]
-					response = await response.json()
-			if "csrf_token" not in response:
-				print(response)
-				raise Exception('Unexpected response')
-			csrf = response['csrf_token']
-			api_key = response['session_api_key']
-			print("================ Credentials ================")
-			print(f'CSRF Token: {csrf}')
-			print(f'Session API Key: {api_key}')
-			print(f'Session cookie: {session_cookie}')
-			print(f'for email {response["email"]}')
-			print()
-		payload = {"operationName": "RadarAllSignalsForChargeQuery",
-		           "variables": {"time": created, "input": {"charge": f'"{charge}"'}},
-		           "query": "query RadarAllSignalsForChargeQuery($time: Int!, $input: ApiRadarSignalsSignalsDataInput) "
-		                    "{\n  currentMerchant {\n    id\n    created\n    radarSettingsAtTime(time: $time) {\n     "
-		                    " highestRiskThreshold\n      elevatedRiskThreshold\n      __typename\n    }\n    "
-		                    "__typename\n  }\n  radarSignalsData(input: $input) {\n    amountMeanForCard\n    "
-		                    "amountStandardDeviationForCard\n    billingToIpDistanceInKm\n    "
-		                    "shippingToBillingDistanceInKm\n    shippingToIpDistanceInKm\n    amountInUsd\n    "
-		                    "issuingBank\n    email\n    cardName\n    deviceBrand\n    deviceModel\n    emailAge\n    "
-		                    "internetServiceProvidor\n    cardholderIp\n    operatingSystem\n    browser\n    "
-		                    "cardholderName\n    cardCountsForDeviceLastWeek {\n      counterValue\n      insightsData "
-		                    "{\n        ...CounterFeatureInsightsDataFragment\n        __typename\n      }\n      "
-		                    "selectedBucket\n      __typename\n    }\n    cardCountsForDeviceSecondsSinceFirstSeen {\n "
-		                    "     counterValue\n      insightsData {\n        ...CounterFeatureInsightsDataFragment\n  "
-		                    "      __typename\n      }\n      selectedBucket\n      __typename\n    }\n    "
-		                    "cardCountsForEmailDeclinesLastWeek {\n      counterValue\n      insightsData {\n        "
-		                    "...CounterFeatureInsightsDataFragment\n        __typename\n      }\n      "
-		                    "selectedBucket\n      __typename\n    }\n    "
-		                    "cardCountsForEmailDeclinesSecondsSinceFirstSeen {\n      counterValue\n      insightsData "
-		                    "{\n        ...CounterFeatureInsightsDataFragment\n        __typename\n      }\n      "
-		                    "selectedBucket\n      __typename\n    }\n    cardCountsForIpLastWeek {\n      "
-		                    "counterValue\n      insightsData {\n        ...CounterFeatureInsightsDataFragment\n       "
-		                    " __typename\n      }\n      selectedBucket\n      __typename\n    }\n    "
-		                    "chargesForIpAllTimeSuccessRate {\n      counterValue\n      insightsData {\n        "
-		                    "...CounterFeatureInsightsDataFragment\n        __typename\n      }\n      "
-		                    "selectedBucket\n      __typename\n    }\n    ipCountsForCardAuthorizedLastWeek {\n      "
-		                    "counterValue\n      insightsData {\n        ...CounterFeatureInsightsDataFragment\n       "
-		                    " __typename\n      }\n      selectedBucket\n      __typename\n    }\n    "
-		                    "ipCountsForCardAuthorizedSecondsSinceFirstSeen {\n      counterValue\n      insightsData "
-		                    "{\n        ...CounterFeatureInsightsDataFragment\n        __typename\n      }\n      "
-		                    "selectedBucket\n      __typename\n    }\n    nameCountsForIpLastWeek {\n      "
-		                    "counterValue\n      insightsData {\n        ...CounterFeatureInsightsDataFragment\n       "
-		                    " __typename\n      }\n      selectedBucket\n      __typename\n    }\n    "
-		                    "nameCountsForIpSecondsSinceFirstSeen {\n      counterValue\n      insightsData {\n        "
-		                    "...CounterFeatureInsightsDataFragment\n        __typename\n      }\n      "
-		                    "selectedBucket\n      __typename\n    }\n    normalizedNameCountsForCardLastWeek {\n      "
-		                    "counterValue\n      insightsData {\n        ...CounterFeatureInsightsDataFragment\n       "
-		                    " __typename\n      }\n      selectedBucket\n      __typename\n    }\n    "
-		                    "countryCountsForCardLastWeek {\n      counterValue\n      insightsData {\n        "
-		                    "...CounterFeatureInsightsDataFragment\n        __typename\n      }\n      "
-		                    "selectedBucket\n      __typename\n    }\n    chargesForEmailAllTimeSuccessRate {\n      "
-		                    "counterValue\n      insightsData {\n        ...CounterFeatureInsightsDataFragment\n       "
-		                    " __typename\n      }\n      selectedBucket\n      __typename\n    }\n    isDebit {\n      "
-		                    "value\n      insightsData {\n        ...CategoricalFeatureInsightsDataFragment\n        "
-		                    "__typename\n      }\n      __typename\n    }\n    isPrepaid {\n      value\n      "
-		                    "insightsData {\n        ...CategoricalFeatureInsightsDataFragment\n        __typename\n   "
-		                    "   }\n      __typename\n    }\n    pastedCvc {\n      value\n      insightsData {\n       "
-		                    " ...CategoricalFeatureInsightsDataFragment\n        __typename\n      }\n      "
-		                    "__typename\n    }\n    pastedEmail {\n      value\n      insightsData {\n        "
-		                    "...CategoricalFeatureInsightsDataFragment\n        __typename\n      }\n      "
-		                    "__typename\n    }\n    pastedExpiration {\n      value\n      insightsData {\n        "
-		                    "...CategoricalFeatureInsightsDataFragment\n        __typename\n      }\n      "
-		                    "__typename\n    }\n    pastedNumber {\n      value\n      insightsData {\n        "
-		                    "...CategoricalFeatureInsightsDataFragment\n        __typename\n      }\n      "
-		                    "__typename\n    }\n    pastedZip {\n      value\n      insightsData {\n        "
-		                    "...CategoricalFeatureInsightsDataFragment\n        __typename\n      }\n      "
-		                    "__typename\n    }\n    isNamePartInEmail {\n      value\n      insightsData {\n        "
-		                    "...CategoricalFeatureInsightsDataFragment\n        __typename\n      }\n      "
-		                    "__typename\n    }\n    hasPriorDisputeOnCardholderIp {\n      value\n      insightsData {"
-		                    "\n        ...CategoricalFeatureInsightsDataFragment\n        __typename\n      }\n      "
-		                    "__typename\n    }\n    hasPriorIfrOnCardholderIp {\n      value\n      insightsData {\n   "
-		                    "     ...CategoricalFeatureInsightsDataFragment\n        __typename\n      }\n      "
-		                    "__typename\n    }\n    __typename\n  }\n}\n\nfragment CounterFeatureInsightsDataFragment "
-		                    "on ApiRadarSignalsCounterSignalBucket {\n  key\n  networkDistribution\n  "
-		                    "normalizedFraudRate\n  __typename\n}\n\nfragment CategoricalFeatureInsightsDataFragment "
-		                    "on ApiRadarSignalsCategoricalSignalBucket {\n  bucketValue\n  networkDistribution\n  "
-		                    "normalizedFraudRate\n  __typename\n}\n"}
-		async with session.post('https://dashboard.stripe.com/ajax/graphql',
-		                        json=payload,
-		                        headers={
-			                        'x-stripe-csrf-token': csrf,
-			                        'stripe-livemode': 'false',
-			                        'Referer': 'https://dashboard.stripe.com/test/payments/pi_1I646ZL5RkOYbcxulqDwH6RR',
-			                        'Origin': 'https://dashboard.stripe.com'
-		                        },
-		                        cookies={
-			                        'session': session_cookie
-		                        }
-		                        ) as response:
-			print(await response.json())
+class InsightLoader:
+    def __init__(self,
+                 email: str,
+                 password: str,
+                 captcha_key: str,
+                 data_sitekey: str = '6LezRwYTAAAAAClbeZahYjeSYHsbwpzjEQ0hQ1jB'):
+        self.email = email
+        self.password = password
+        self.captcha_solver = Captcha(captcha_key)
+
+        self.__is_logged_in = False
+        self.__session = None
+        self.__csrf = None
+        self.__session_cookie = None
+
+    async def sign_in(self):
+        session = ClientSession(headers={
+            'User-Agent': USER_AGENT
+        })
+        async with session.get('https://dashboard.stripe.com/login',
+                               ) as response:
+            await response.read()
+            unesc_csrf = str(response.headers).split('stripe.csrf=')[1].split('; domain=stripe.com')[0]
+            csrf = parse.unquote(unesc_csrf)
+        # Defining login payload
+        payload = {
+            "email": EMAIL,
+            "password": PASSWORD,
+            "io_blackbox": "",
+            "remember": True,
+            "merchant": "",
+            "invite_code": "",
+            "account_invite": "",
+            "invite": "",
+            "redirect": "/",
+            "source": "main_login",
+            "has_platform_authenticator": False
+        }
+        # Defining headers
+        headers = {
+            'origin': 'https://dashboard.stripe.com',
+            'referer': 'https://dashboard.stripe.com/login',
+            'x-stripe-csrf-token': csrf,
+            'x-requested-with': 'XMLHttpRequest'
+        }
+        async with session.post('https://dashboard.stripe.com/ajax/sessions',
+                                headers=headers,
+                                data=payload) as response:
+            response = await response.json()
+            if response.get('error_type', '') == 'need_captcha':
+                # Solve captcha if it raised
+                captcha = Captcha(CAPTCHA_KEY)
+                g_response = captcha.solve('https://dashboard.stripe.com/login',
+                                           DATA_SITEKEY)
+                payload['g-recaptcha-response'] = g_response
+                async with session.post('https://dashboard.stripe.com/ajax/sessions',
+                                        headers=headers,
+                                        data=payload) as response:
+                    self.__session_cookie = str(response.headers).split('session=')[1].split('; domain')[0]
+                    response = await response.json()
+            if "csrf_token" not in response:
+                print(response)
+                raise Exception('Unexpected response')
+            self.__session = session
+            self.__csrf = response['csrf_token']
+            self.__is_logged_in = True
+
+    async def load_one(self,
+                       charge: str,
+                       created: int):
+        if not self.__is_logged_in:
+            raise Exception('Stripe was never logged in')
+        payload = risk_graphql(created, charge)
+        async with self.__session.post('https://dashboard.stripe.com/ajax/graphql',
+                                       json=payload,
+                                       headers={
+                                           'x-stripe-csrf-token': self.__csrf,
+                                           'stripe-livemode': 'false',
+                                           'Referer': 'https://dashboard.stripe.com/test/payments/'
+                                                      'pi_1I646ZL5RkOYbcxulqDwH6RR',
+                                           'Origin': 'https://dashboard.stripe.com'
+                                       },
+                                       cookies={
+                                           'session': self.__session_cookie
+                                       }
+                                       ) as response:
+            return await response.json()
+
+    async def load_many(self,
+                        payments: list,
+                        parallel: int = 3):
+        chunk_list = split(payments, parallel)
+        result = list()
+        for chunk in chunk_list:
+            result += await asyncio.gather(*[self.load_one(*element) for element in chunk])
+        return result
+
+    async def close(self):
+        await self.__session.close()
 
 
-@click.command()
-@click.option('--charge', default=CHARGE)
-@click.option('--created', type=int, default=CREATED)
-def click_main(charge: str,
-               created: int):
-	asyncio.get_event_loop().run_until_complete(main(charge, created))
+async def test():
+    loader = InsightLoader(EMAIL, PASSWORD, CAPTCHA_KEY)
+    await loader.sign_in()
+    # for one - await loader.load_one(charge, created)
+    result = await loader.load_many([
+        [CHARGE, CREATED],
+        [CHARGE, CREATED],
+        [CHARGE, CREATED]
+    ])
+    print(result)
+    await loader.close()
 
 
 if __name__ == "__main__":
-	click_main()
+    asyncio.get_event_loop().run_until_complete(test())
